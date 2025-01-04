13b9abd8dd2734b5269ab323b521d86efcfeb942
Malak <malakbouyi8@gmail.com>
2025-01-04 13:30:02 +0100

    <3

---
+++ get_next_line_bonus.c
@@ -0,0 +1,74 @@
+#include "get_next_line_bonus.h"
+char *get_buffer(int fd,char *res)
+{
+    int i;
+    char *buff;
+    buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+    if(!buff)
+        return (NULL);
+    i = 1;
+    while(i > 0)
+    {
+        i = read(fd,buff,BUFFER_SIZE);
+        if(i == -1)
+        {
+            free(buff);
+            free(res);
+            return (NULL);
+        }
+        if(i == 0)
+            break;
+        buff[i] = '\0';
+        res = ft_strjoin(res,buff);
+        if(ft_strchr(buff,'\n'))
+            break;
+    }
+    free(buff);
+    return (res);
+}
+char *get_f(char *res)
+{
+    int i;
+    char *line;
+
+    i = 0;
+    while(res[i] && res[i] != '\n')
+        i++;
+    if(res[i] == '\n')
+        i++;
+    line = ft_substr(res,0,i);
+    return(line);
+}
+char *get_n(char *res)
+{
+    int i;
+    char *line;
+
+    i = 0;
+    while(res[i] && res[i] != '\n')
+        i++;
+    if(res[i] == '\n')
+        i++;
+    line = ft_substr(res,i,ft_strlen(res) - i);
+    free(res);
+    return(line);
+}
+char *get_next_line(int fd)
+{
+    static char *res[1024];
+    char *line;
+    
+    if(fd < 0 || BUFFER_SIZE <= 0 )
+        return (NULL);
+    res[fd] = get_buffer(fd,res[fd]);
+    if (!res[fd] || *res[fd] == '\0')
+    {
+        if (res[fd])
+            free(res[fd]);
+        res[fd] = NULL;
+        return (NULL);
+    }
+    line = get_f(res[fd]);
+    res[fd] = get_n(res[fd]);
+    return(line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE__BONUS_H
+#define GET_NEXT_LINE_BONUS_H 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 2
+#endif
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+#include <stdio.h> 
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlen(const char *str);
+char *get_buffer(int fd,char *result);
+char *get_f(char *res);
+char *get_n(char *res);
+char *get_next_line(int fd);
+
+#endif
